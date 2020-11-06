bdb8bdea9f758b9dd27c3f14ef5ba61284447d6a
dmorris6-hub <74027372+dmorris6-hub@users.noreply.github.com>
2020-11-05 19:42:18 -0500

    Origin-0

---
+++ src/firebase/config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDU3HdOEbl9Vi3dHUXjrFBI9_JpAnFP278",
+    authDomain: "tlxbase.firebaseapp.com",
+    databaseURL: "https://tlxbase.firebaseio.com",
+    projectId: "tlxbase",
+    storageBucket: "tlxbase.appspot.com",
+    messagingSenderId: "1095622256209",
+    appId: "1:1095622256209:web:5a15f4b28402fa1bba7a70"
+};
